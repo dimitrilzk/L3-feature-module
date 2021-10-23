fdb210ca8892ebe253e1138d51c7b855316ce26c
Nicola Toledo <hello@nicolatoledo.dev>
2021-10-23 12:09:44 +0200

    rename

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/esercizio1'),
+      dir: require('path').join(__dirname, './coverage/epicode-esercizi'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'esercizio1'`, () => {
+  it(`should have as title 'epicode-esercizi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('esercizio1');
+    expect(app.title).toEqual('epicode-esercizi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('esercizio1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('epicode-esercizi app is running!');
   });
 });
